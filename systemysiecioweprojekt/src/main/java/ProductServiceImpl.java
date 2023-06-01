42eeb32780ceb706507f97b91eb53d5ae2f1fabf
Bartosz Grygierczyk <bartoszgrygierczyk01@gmail.com>
2023-06-01 19:12:03 +0200

    kofiguracja springa

--- systemysiecioweprojekt/src/main/java/ProductServiceImpl.java
+++ systemysiecioweprojekt/src/main/java/ProductServiceImpl.java
@@ -1,35 +1,44 @@
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.grygierczyk.restws.ProductService;
 import com.grygierczyk.restws.entities.Product;
+import com.grygierczyk.restws.repos.ProductRepository;
 
 import jakarta.ws.rs.core.Response;
 
 public class ProductServiceImpl implements ProductService {
 
+	@Autowired
+	ProductRepository repository;
+	
 	@Override
 	public List<Product> getProducts() {
-		return null;
+		return repository.findAll();
 	}
 
 	@Override
 	public Product getProduct(int id) {
-		return null;
+		return repository.findById(id).get();
 	}
 
 	@Override
 	public Response createProduct(Product product) {
-		return null;
+		Product saveProduct = repository.save(product);
+		return Response.ok(saveProduct).build();
 	}
 
 	@Override
 	public Response updateProduct(Product product) {
-		return null;
+		Product saveProduct = repository.save(product);
+		return Response.ok(saveProduct).build();
 	}
 
 	@Override
 	public Response deleteProduct(Product product) {
-		return null;
+		repository.delete(product);
+		return Response.ok().build();
 	}
 
 }
